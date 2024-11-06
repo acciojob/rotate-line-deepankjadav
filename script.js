8d3d052f3f4f6dff4b32e65c8288ca952c82f72c
deepankjadav <jadavdeepank@gmail.com>
2024-11-06 21:07:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+let angle = 0;
+const line = document.getElementById('line');
+
+function rotateLine() {
+    angle += 2;
+    line.style.transform = `rotate(${angle}deg)`;
+    requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
